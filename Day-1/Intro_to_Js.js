70f6718a8ac9d01eba6ad808986cc2a5583ef110
Gaurav Kumar Sharma <gauravjabbal@gmail.com>
2023-10-22 08:28:44 +0530

    day 1 task completed

---
+++ Day-1/Intro_to_Js.js
@@ -0,0 +1,50 @@
+// Prob1 // Print "Masai School" in the console followed by "A Transformation in Education" in next line
+console.log("Masai")
+console.log("A Transformation in Education")
+
+// Prob2 Define a variable called name with your Name as the assigned value
+// Print the value stored in the variable name
+// Change the variable to store your father's name
+// Print the value stored in the variable name
+// Change the variable again to store your mother's name.
+// Print the value stored in the variable name
+
+let name ="Gaurav"
+console.log(name)
+let F_name ="Jitender Kumar"
+console.log(F_name)
+let M_name ="Suntia Devi"
+console.log(M_name)
+
+
+// Prob3 Create a variable name to store your name
+// Create a variable age to store your age
+// Print the values stored in the variable on one line followed by the type of the variable in the next line
+
+let name1 ="Gaurav"
+let age =23
+console.log(type(name1),name1)
+console.log(type(age), age)
+
+// prob4 Store the name, school, grade, section, rollno and the marks scored by the student in 3 subjects
+// Print the report card of the student (You can make it look nice by using some keyboard symbols )
+// Explore ASCII ART (https://en.wikipedia.org/wiki/ASCII_art (Links to an external site.)) or 
+// Text Art (https://fsymbols.com/text-art/ (Links to an external site.)) for some inspiration
+
+let name2 = `𝙂𝙖𝙪𝙧𝙖𝙫`;
+let School = "Masai";
+let Grade = "A";
+let rollno = 197;
+let English = 78;
+let Science = 89;
+let Hindi = 96;
+
+console.log("############# Report card  ##############");
+console.log("##", "Name:",name2,"                       ##");
+console.log("##","School:",School,"                      ##");
+console.log("##","Grade:",Grade,"                           ##");
+console.log("##","Rollno.:",rollno,"                       ##");
+console.log("##","English:",English,"                        ##");
+console.log("##","Science:",Science,"                        ##");
+console.log("##","Hindi:",Hindi,"                          ##");
+console.log("#########################################");
